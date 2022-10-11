52aaef1d1310fa7be6a1cf403a23297cf1d2355c
程明旺 <chengmingwang@bytedance.com>
2022-10-11 18:26:02 +0800

    feat: 盛最多水的容器

---
+++ src/11.盛最多水的容器/index.ts
@@ -0,0 +1,17 @@
+﻿function maxArea(height: number[]): number {
+  let result = 0
+  let left = 0
+  let right = height.length - 1
+  while (left < right) {
+    result = Math.max(
+      (right - left) * Math.min(height[right], height[left]),
+      result
+    )
+    if (height[left] >= height[right]) {
+      right--
+    } else {
+      left++
+    }
+  }
+  return result
+}
